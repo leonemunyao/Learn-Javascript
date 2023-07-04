c0cc8d24309914cc90bd0cf2357f7d3876924052
leonemunyao <leonemunyao@gmail.com>
2023-07-04 20:30:53 +0300

    Changes

---
+++ Main.js
@@ -0,0 +1,19 @@
+// In computer science data is anything that the computer understands
+// We have to understand what we have differen data types like undefined...null.....boolean...string
+// symbol....number..bigit and object
+var myName;
+var myName = Leon;
+var myVar = 10;
+
+
+// assinging variables
+var a;
+a = 7;
+var b;
+b = a;
+
+// Initializing Variables with the Assignment Operator
+
+var myVar = 0;
+var a = 9;
+
