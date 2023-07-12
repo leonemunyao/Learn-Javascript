666948fb712920c0a742f1cf5c3217587f5493c1
leonemunyao <leonemunyao@gmail.com>
2023-07-12 21:33:42 +0300

    accessing multi-dimensional arrays

--- Main.js
+++ Main.js
@@ -145,6 +145,30 @@ const myAArray = [18, 64, 99];
 
 myAArray[0] = 45;
 
+// Access Multi-Dimensional Arrays With Indexes
+
+const arr = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [[10, 11, 12], 13, 14]
+];
+
+const subarray = arr[3];
+const nestedSubarray = arr[3][0];
+const element = arr[3][0][1];
+
+// Example 2  
+// Will print 8 
+const myArraay = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [[10, 11, 12], 13, 14],
+];
+
+const myDatta = myArraay[2][1];
+
 
 
 
