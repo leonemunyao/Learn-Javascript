399c12e67dc819df1da7f56cdefc27bba16868ae
leonemunyao <leonemunyao@gmail.com>
2023-07-14 21:20:10 +0300

    Global Functions

--- Main.js
+++ Main.js
@@ -272,3 +272,23 @@ const value = timesFive(10);
 // Variables which are declared without let or const are automatically created in the global scope. This means they can be 
 // seen everywhere in your javascript code.
 
+const myGlobal = 10; 
+
+function fun1() {
+  // Assign 5 to oopsGlobal here
+  oopsGlobal = 5;
+
+}
+
+// Only change code above this line
+
+function fun2() {
+  let output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
