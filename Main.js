65ee44d1841fd70b947d6be383425bb2d45139c7
leonemunyao <leonemunyao@gmail.com>
2023-07-19 23:59:19 +0300

    <= operator

--- Main.js
+++ Main.js
@@ -472,6 +472,22 @@ console.log(testLessThan(10));
 
 
 
+function testLessOrEqual(val) {
+  if (val <= 12 ) {  
+    return "Smaller Than or Equal to 12";
+  }
+
+  if (val <= 24 ) {  
+    return "Smaller Than or Equal to 24";
+  }
+
+  return "More Than 24";
+}
+
+console.log(testLessOrEqual(10));
+
+
+
 
 
 
