920705655e46136e1575c52bec540f34c9c8cd83
leonemunyao <leonemunyao@gmail.com>
2023-07-19 23:56:53 +0300

    < operator

--- Main.js
+++ Main.js
@@ -455,6 +455,21 @@ function testGreaterOrEqual(val) {
 console.log(testGreaterOrEqual(10));
 
 
+// Comparison with the Less Than Operator
+function testLessThan(val) {
+  if (val < 25 ) {  
+    return "Under 25";
+  }
+
+  if (val < 55) {
+    return "Under 55";
+  }
+
+  return "55 or Over";
+}
+
+console.log(testLessThan(10));
+
 
 
 
