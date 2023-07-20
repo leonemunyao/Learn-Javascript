bebfa33c16f56653449d0353133db3c2ed4c5194
leonemunyao <leonemunyao@gmail.com>
2023-07-20 11:19:02 +0300

    || operator , if else statements

--- Main.js
+++ Main.js
@@ -557,6 +557,33 @@ function testElse(val) {
 console.log(testElse(4));
 
 // Introducing Else If Statements
+function ifElse(num) {
+  if (num > 15) {
+    return "Bigger than 15";
+  } else if (num < 5) {
+    return "Smaller than 5";
+  } else {
+    return "Between 5 and 15";
+  }
+}
+console.log(ifElse(80));
+
+
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  }
+
+  else if (val < 5) {
+    return "Smaller than 5";
+  }
+  else {
+    return "Between 5 and 10";
+  }
+}
+
+console.log(testElseIf(7));
+
 
 
 
