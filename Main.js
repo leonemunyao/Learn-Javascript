1c619d397c3a6f6d3f063340911420c88e17b46a
leonemunyao <leonemunyao@gmail.com>
2023-07-24 09:37:58 +0300

    Counting cards

--- Main.js
+++ Main.js
@@ -741,6 +741,65 @@ function sequentialSizes(val) {
 
 console.log(sequentialSizes(1));
 
+// Returning Boolean Values from Functions
+function isEqual(a, b) {
+  if (a === b) {
+    return true;
+  } else {
+    return false;
+  }
+}
+// This is written as follows to remove the if else statement
+function isEqual(a, b) {
+  return a === b;
+}
+
+function isLess(a, b) {
+    return a < b;
+}
+console.log(isLess(15, 10));
+
+// Return Early Pattern for Functions
+function myFun() {
+  console.log("Hello");
+  return "World";
+  console.log("byebye")   // Byebye won't be displayed because the function exits at the return statement.
+}
+myFun();
+
+// Counting Cards
+
+let count = 0;
+
+function cc(card) {
+   switch(card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count--;
+      break;
+  }
+
+  var holdbet = 'Hold'
+  if (count > 0) {
+    holdbet = 'Bet'
+  }
+
+  return count + " " + holdbet;
+
+}
+
+console.log(cc(2), cc(3), cc(7), cc('K'), cc('A'));
+
 
 
 
