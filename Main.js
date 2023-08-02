f7dff7acf775090cd7b871adc04bbf0ccc35cb6b
leonemunyao <leonemunyao@gmail.com>
2023-08-02 22:24:25 +0300

    Nested Loops

--- Main.js
+++ Main.js
@@ -1304,3 +1304,17 @@ for (let i = 0; i < arr10.length; i++) {
     console.log(arr10[i][j]);
   }
 }
+
+//Modify function multiplyAll so that it returns the product of all the numbers in the sub-arrays of arr.
+
+function multiplyAll(arr) {
+  let product = 1;
+  for(let i = 0; i < arr.length; i++) {
+    for(let j = 0; j < arr[i].length; j++){
+      product *= arr[i][j];
+    }
+  }
+  return product;
+}
+
+console.log(multiplyAll([[1, 2], [3, 4], [5, 6, 7]]));
